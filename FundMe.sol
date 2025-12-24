fb94653b3bd0a622ca89cf320dbf0d1abc3774d2
Uy Scutty <abeg0703@gmail.com>
2025-12-24 23:51:41 +0000

    Baic operational FundMe contract

--- FundMe.sol
+++ FundMe.sol
@@ -10,36 +10,43 @@ contract FundMe {
     AggregatorV3Interface public immutable priceFeed;
 
 
-    address owner;
+    address immutable owner;
 
     constructor(address _chainAddress) {// address  get this from chainlink  that's what am passing
         owner = msg.sender;
         priceFeed= AggregatorV3Interface(_chainAddress);
     } 
 
-    mapping (address => uint256 ) public ammountFunded; // keeps tract of how much each address donates
+    mapping (address => uint256 ) public amountFunded; // keeps tract of how much each address donates
     address [] public funders; // stores all donors address
 
 
 
-    function fund (uint256 _amount) public payable  {
-        require (_amount > minimumUsd, "Not enough of ETH");
-        ammountFunded[msg.sender] += msg.value;
+    function fund () public payable  {
+        uint256 ethInUsd =  getConversion(msg.value); // takes the amount of eth i want to deposit and converts to dollars
+        require ( ethInUsd >= minimumUsd * 1e18, "Not enough of ETH"); // compares that dollar to set minimun
+        amountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
 
-    function getPrice  () view  public returns (int256 price) {
-        
-        // ABI
+    function getPrice  () view  public returns (int256) {
         (, int256 price, , ,)= priceFeed.latestRoundData ();
         return price;
     }
 
-    function getConversion ()  view public {
-        
+    function getConversion (uint256 _ethAmount)  view public returns (uint256) {
+        int256 rawPrice = getPrice();
+        require(rawPrice > 0, "Invalid Price");
+
+        uint256 price = uint256(rawPrice);
+
+        return (price * _ethAmount) / 1e8;
     }
 
-    function withdraw () view public {
-        require (msg.sender == owner, "Only admin can withdraw funds");
+
+    function withdraw () public {
+        require(msg.sender == owner, "You don't own this contract");
+        payable(owner).transfer(address(this).balance);
+
     }
 }
